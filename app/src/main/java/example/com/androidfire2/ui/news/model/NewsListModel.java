be950d33cde4226ddd00dfe5794b46b01d59e4a6
zjl7577 <1097919195@qq.com>
2018-12-03 17:18:11 +0800

    获取列表时的操作符优化

--- app/src/main/java/example/com/androidfire2/ui/news/model/NewsListModel.java
+++ app/src/main/java/example/com/androidfire2/ui/news/model/NewsListModel.java
@@ -1,6 +1,8 @@
 package example.com.androidfire2.ui.news.model;
 
+import com.google.gson.Gson;
 import com.jaydenxiao.common.baserx.RxSchedulers;
+import com.jaydenxiao.common.commonutils.LogUtils;
 import com.jaydenxiao.common.commonutils.TimeUtil;
 
 import java.util.List;
@@ -28,28 +30,32 @@ public class NewsListModel implements NewsListContract.Model {
                 .flatMap(new Func1<Map<String, List<NewsSummary>>, Observable<NewsSummary>>() {
                     @Override
                     public Observable<NewsSummary> call(Map<String, List<NewsSummary>> map) {
+                        LogUtils.loge(map.keySet().toString());//打印key即对应的id
+                        LogUtils.loge(new Gson().toJson(map));
                         if (id.endsWith(ApiConstants.HOUSE_ID)) {
+                            LogUtils.loge(new Gson().toJson(map));
                             // 房产实际上针对地区的 它的id与返回key不同
-                            return Observable.from(map.get("北京"));//form依次发送 map.get("北京")
+                            return Observable.from(map.get("北京"));//form依次发送 map.get("北京")  这里是房产的话返回的key是北京，所以一定要flatmap一下
                         }
                         return Observable.from(map.get(id));//form依次发送 map.get(id)
                     }
                 })
                 //转化时间(.map()事件 对象的直接变换。所谓变换，就是将事件序列中的对象或整个序列进行加工处理，转换成不同的事件或事件序列。)（一对一）
-                .map(new Func1<NewsSummary, NewsSummary>() {
-                    @Override
-                    public NewsSummary call(NewsSummary newsSummary) {
-                        String ptime = TimeUtil.formatDate(newsSummary.getPtime());
-                        newsSummary.setPtime(ptime);
-                        return newsSummary;
-                    }
-                })
+//                .map(new Func1<NewsSummary, NewsSummary>() {
+//                    @Override
+//                    public NewsSummary call(NewsSummary newsSummary) {
+//                        String ptime = TimeUtil.formatDate(newsSummary.getPtime());
+//                        newsSummary.setPtime(ptime);
+//                        return newsSummary;
+//                    }
+//                })
                 .distinct()//去重(删除重复项)
-                //toSortList 类似于toList ,不同的 他能实现 对 列表进行排序 , 默认是自然升序, 数据项必须实现Comparable接口
+                //toSortList 类似于toList ,不同的 他能实现 对 列表进行排序 , 默认是自然升序, 数据项必须实现Comparable接口   最后以多个NewsSummary即List<NewsSummary>
                 .toSortedList(new Func2<NewsSummary, NewsSummary, Integer>() {
                     @Override
                     public Integer call(NewsSummary newsSummary, NewsSummary newsSummary2) {
-                        return newsSummary2.getPtime().compareTo(newsSummary.getPtime());
+                        //compareTo如果指定的数与参数相等返回0;如果指定的数小于参数返回 -1;如果指定的数大于参数返回 1;
+                        return newsSummary2.getPtime().compareTo(newsSummary.getPtime());//>0 升序 ，<0 降序
                     }
                 })
                 //声明线程调度
